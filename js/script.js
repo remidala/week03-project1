ac1d05ade0c4d16acfdda3fb9662dc721b26a7b7
Mihaly Reiter <100424934+remidala@users.noreply.github.com>
2022-07-26 16:51:40 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const input = document.getElementsByTagName('input');
+
+
+const welcome = document.getElementById('welcome');
+const generator = document.getElementById('generator');
+const head = document.getElementById('keycode');
+
+function keycode(event) {
+const key = event.key;
+const location = event.location;
+const which = event.which;
+const code = event.code;
+
+head.textContent = which;
+document.getElementById("eventKey").innerHTML= key;
+document.getElementById("eventLocation").innerHTML= location;
+document.getElementById("eventWhich").innerHTML= which;
+document.getElementById("eventCode").innerHTML= code;
+generator.classList.remove("show");
+welcome.remove()
+};
+
+
+ 
+ 
